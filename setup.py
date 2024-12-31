d0353ee03bb74577e29edc7259b9e40209dab900
Sopanha2020 <62559056+Sopanha2020@users.noreply.github.com>
2024-12-31 10:57:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,3 +2,14 @@ from distutils.core import setup
 import py2exe
 
 setup(console=['bmi_calculator.py'])
+
+***
+from distutils.core import setup
+import py2exe
+from win32com.shell import shell
+
+setup(
+    windows=[{'script': 'bmi_calculator.py', 'icon_resources': [(1, "C:\\Users\\User\\Documents\\GitHub\\BMI-Calculator\\my_icon.ico")]}],
+    options={'py2exe': {'bundle_files': 1}} 
+)
+***
